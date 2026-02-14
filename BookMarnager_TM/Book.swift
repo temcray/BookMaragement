93385b61f4525404b9b99eb964122e6be39fb7f3
Tatiana Mccray <ejamison06@gmail.com>
2026-02-14 12:35:13 -0600

    tm

---
+++ BookMarnager_TM/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  BookMarnager_TM
+//
+//  Created by Tatiana6mo on 2/12/26.
+//
+
+import SwiftUI
+
+struct Book: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Book()
+}
