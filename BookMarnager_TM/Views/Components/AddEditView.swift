c8bd4104975322b82a74fe1f30be96f0eea92fff
Tatiana Mccray <ejamison06@gmail.com>
2026-02-20 12:52:10 -0600

    assingment 3

---
+++ BookMarnager_TM/Views/Components/AddEditView.swift
@@ -0,0 +1,18 @@
+//
+//  AddEditView.swift
+//  BookMarnager_TM
+//
+//  Created by Tatiana6mo on 2/20/26.
+//
+
+import SwiftUI
+
+struct AddEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddEditView()
+}
