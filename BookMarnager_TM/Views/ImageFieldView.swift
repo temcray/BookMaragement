eaa2feaa2240b9063aa11b0284776b0e3f3911f0
Tatiana Mccray <ejamison06@gmail.com>
2026-03-05 22:08:23 -0600

    assignment 4

--- BookMarnager_TM/Views/ImageFieldView.swift
+++ BookMarnager_TM/Views/ImageFieldView.swift
@@ -1,18 +1,18 @@
 //
-//  api.swift
+//  ImageFieldView.swift
 //  BookMarnager_TM
 //
-//  Created by Tatiana6mo on 2/17/26.
+//  Created by Tatiana6mo on 3/5/26.
 //
 
 import SwiftUI
 
-struct api: View {
+struct ImageFieldView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    api()
+    ImageFieldView()
 }
